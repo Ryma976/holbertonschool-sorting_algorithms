0fc5d10ed995096b80bbe5de48e56c28741f291e
reem <ryma6110@gmail.com>
2026-04-30 15:08:59 +0000

    Fix directory structure and add all files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,19 @@
 #include "sort.h"
-
-/**
- * bubble_sort - Sorts an array of integers in ascending order
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
-void bubble_sort(int *array, size_t size)
-{
-	size_t i, len = size;
-	int tmp;
-	int swapped = 1;
-
-	if (array == NULL || size < 2)
-		return;
-
-	while (swapped)
-	{
-		swapped = 0;
-		for (i = 0; i < len - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				swapped = 1;
-				print_array(array, size);
-			}
-		}
-		/* Optimization: the largest element is already at the end */
-		len--;
-	}
+void bubble_sort(int *array, size_t size) {
+    size_t i, j;
+    int temp, swapped;
+    if (array == NULL || size < 2) return;
+    for (i = 0; i < size - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+                swapped = 1;
+            }
+        }
+        if (swapped == 0) break;
+    }
 }
