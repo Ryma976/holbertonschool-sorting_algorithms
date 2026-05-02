ce263f082ef8a2ae8af1a7d1f9d33a1aff92617c
tala2277 <tt1341700@gmail.com>
2026-05-02 17:20:33 +0000

    Add sort header

--- sort.h
+++ sort.h
@@ -1,11 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -19,12 +19,6 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
